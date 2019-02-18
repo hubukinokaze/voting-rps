26ff49a8cc6dfd9e5b91303db9a2e0a6fd420077
hubukinokaze <yudai.j.kawa@gmail.com>
2019-02-17 20:11:55 -0800

    +Updated logic for ai selecting rng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { MatSnackBar } from "@angular/material";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title: string    = 'Voting RPS';
   deck: Array<any> = [];
+  isGameOver: boolean;
   player1: Player  = new Player();
   player2: Player  = new Player();
 
@@ -18,11 +19,17 @@ export class AppComponent {
   }
 
   ngOnInit() {
+    this.startGame();
+  }
+
+  public startGame() {
+    this.isGameOver = false;
     this.createDeck();
     this.passCards(this.player1);
     this.passCards(this.player2);
-    this.selectCard(this.player2, 1);
+    this.selectCard(this.player2, Math.floor(Math.random() * 3));
     this.player1.isTurn = true;
+    this.player2.isTurn = false;
   }
 
   private createDeck() {
@@ -66,37 +73,41 @@ export class AppComponent {
   }
 
   public selectCard(player: Player, index: number) {
-    console.log(player);
-    for (let i = 0; i < player.hand.length; i++) {
-      if (i != index) {
-        player.hand[i].isSelected = false;
+    if (!this.isGameOver) {
+      for (let i = 0; i < player.hand.length; i++) {
+        if (i != index) {
+          player.hand[i].isSelected = false;
+        }
       }
+      player.hand[index].isSelected = !player.hand[index].isSelected;
+      player.isSelected             = (player.hand[index].isSelected) ? true : false;
     }
-    player.hand[index].isSelected = !player.hand[index].isSelected;
-    player.isSelected             = (player.hand[index].isSelected) ? true : false;
   }
 
   public submit() {
-    let msg = '';
-    let selectedCard = this.player1.hand.filter( (x) => x.isSelected == true)[0].name;
-    let enemySelectedCard = this.player2.hand.filter( (x) => x.isSelected == true)[0].name;
+    if (!this.isGameOver) {
+      let msg = '';
+      let selectedCard = this.player1.hand.filter( (x) => x.isSelected == true)[0].name;
+      let enemySelectedCard = this.player2.hand.filter( (x) => x.isSelected == true)[0].name;
 
-    if (selectedCard === enemySelectedCard) {
-      msg = 'You tied!';
-    } else if (selectedCard === 'ROCK' && enemySelectedCard === 'PAPER') {
-      msg = 'You lose!';
-    } else if (selectedCard === 'PAPER' && enemySelectedCard === 'SCISSOR') {
-      msg = 'You lose!';
-    } else if (selectedCard === 'SCISSOR' && enemySelectedCard === 'ROCK') {
-      msg = 'You lose!';
-    } else {
-      msg = 'You win!';
+      if (selectedCard === enemySelectedCard) {
+        msg = 'You tied!';
+      } else if (selectedCard === 'ROCK' && enemySelectedCard === 'PAPER') {
+        msg = 'You lose!';
+      } else if (selectedCard === 'PAPER' && enemySelectedCard === 'SCISSOR') {
+        msg = 'You lose!';
+      } else if (selectedCard === 'SCISSOR' && enemySelectedCard === 'ROCK') {
+        msg = 'You lose!';
+      } else {
+        msg = 'You win!';
+      }
+      this.player1.isTurn = false;
+      this.player2.isTurn = true;
+      this.isGameOver = true;
+
+      this.snackBar.open(msg, '', {
+        duration: 5000,
+      });
     }
-    this.player1.isTurn = false;
-    this.player2.isTurn = true;
-
-    this.snackBar.open(msg, '', {
-      duration: 5000,
-    });
   }
 }
